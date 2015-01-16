ab464f1d44a64829576d56f5663b289b8d814d1f
david.villalobos.c <david.villalobos.c@gmail.com>
2015-01-16 13:39:38 +0000

    * NEW: Add a new Helper.

--- staticfunctions.cpp
+++ staticfunctions.cpp
@@ -1599,6 +1599,7 @@ QStringList StaticFunctions::mariadbHelper()
       << "CHANGE MASTER TO MASTER_BIND = 'interface_name', MASTER_HOST = 'host_name', MASTER_USER = 'user_name', MASTER_PASSWORD = 'password', MASTER_PORT = port_num, MASTER_CONNECT_RETRY = interval, MASTER_HEARTBEAT_PERIOD = interval, MASTER_LOG_FILE = 'master_log_name', MASTER_LOG_POS = master_log_pos, RELAY_LOG_FILE = 'relay_log_name', RELAY_LOG_POS = relay_log_pos, MASTER_SSL = {0|1}, MASTER_SSL_CA = 'ca_file_name', MASTER_SSL_CAPATH = 'ca_directory_name', MASTER_SSL_CERT = 'cert_file_name', MASTER_SSL_KEY = 'key_file_name', MASTER_SSL_CIPHER = 'cipher_list', MASTER_SSL_VERIFY_SERVER_CERT = {0|1}, IGNORE_SERVER_IDS = [server_id [, server_id] ... ]"
       << "CHECK TABLE tbl_name [, tbl_name] {FOR UPGRADE | QUICK | FAST | MEDIUM | EXTENDED | CHANGED}"
       << "CHECKSUM TABLE tbl_name [, tbl_name] [QUICK | EXTENDED]"
+      << "CREATE DATABASE `DatabaseName` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;"
       << "DELETE FROM"
       << "DROP DATABASE IF EXISTS"
       << "DROP DATABASE"
